c5b25be281e2c7d3ee63dce8db4dbbb4fa9f6474
Arunkumar <arunthangaraj11@gmail.com>
2024-06-30 22:30:19 -0400

    created tables for models

--- src/main/java/com/example/productcatalogservice/models/BaseModel.java
+++ src/main/java/com/example/productcatalogservice/models/BaseModel.java
@@ -1,16 +1,29 @@
 package com.example.productcatalogservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.util.Date;
 
 @Getter
 @Setter
-
+@MappedSuperclass
 public abstract class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @CreatedDate
     private Date createdAt;
+    @LastModifiedDate
     private Date modifiedAt;
+    @CreatedBy
+    private String createdBy;
     private Status status;
 }
--- src/main/java/com/example/productcatalogservice/models/Product.java
+++ src/main/java/com/example/productcatalogservice/models/Product.java
@@ -2,6 +2,10 @@ package com.example.productcatalogservice.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,11 +13,13 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-//@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
 public class Product extends BaseModel{
     private String name;
     private String description;
     private String imageUrl;
     private Double price;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 }
